bfb672b1456536f8709c1ca4ff189c1178456b66
Vashisht Gupta <guptavashisht24@gmail.com>
2019-12-02 21:52:15 +0530

    add images

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import "./App.css";
+
+const Card = () => {
+  return(<>
+  <div className = "full">
+    <div className = "card">
+
+    </div>
+    <div className = "form">
+
+    </div>
+  </div>
+  </>)
+}
+
+
+export default Card;
