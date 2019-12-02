f77d114f17603016c95f288a34b7a5bd00571a87
Vashisht Gupta <guptavashisht24@gmail.com>
2019-12-02 22:42:11 +0530

    add css for flex

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,15 @@ const Card = () => {
   return(<>
   <div className = "full">
     <div className = "card">
-
+      <div className = "logo-row w100">
+        Vashisht
+      </div> 
+      <div className = "num-row w100">
+        Vashisht
+      </div> 
+      <div className = "info-row w100">
+        Vashisht
+      </div> 
     </div>
     <div className = "form">
 
