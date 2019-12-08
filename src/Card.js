648263306581cd5d9d0438e43bf06bdbd4b61da6
Vashisht Gupta <guptavashisht24@gmail.com>
2019-12-08 23:53:52 +0530

    add data

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,15 @@
 import React  from 'react';
 import "./App.css";
+import Chip from './assets/chip.png';
+import Amex from './assets/amex.png';
 
 const Card = () => {
   return(<>
   <div className = "full">
     <div className = "card">
       <div className = "logo-row w100">
-        Vashisht
+        <div><img src = {Chip} className = "w70 h55"></img></div>
+        <div><img src = {Amex} className = "w70 h55"></img></div>
       </div> 
       <div className = "num-row w100">
         Vashisht
