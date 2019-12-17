c02bf0e9f15277d03af8a5eacfd1b3fd208c1dbd
Vashisht Gupta <guptavashisht24@gmail.com>
2019-12-17 23:40:09 +0530

    Add 3d shizzz

--- src/Card.js
+++ src/Card.js
@@ -1,27 +1,42 @@
-import React  from 'react';
+import React from 'react';
 import "./App.css";
 import Chip from './assets/chip.png';
 import Amex from './assets/amex.png';
 
 const Card = () => {
-  return(<>
-  <div className = "full">
-    <div className = "card">
-      <div className = "logo-row w100">
-        <div><img src = {Chip} className = "w70 h55"></img></div>
-        <div><img src = {Amex} className = "w70 h55"></img></div>
-      </div> 
-      <div className = "num-row w100">
-        Vashisht
-      </div> 
-      <div className = "info-row w100">
-        Vashisht
-      </div> 
-    </div>
-    <div className = "form">
+  return (<>
+    <div className="full">
+      <div className="card">
+        <div className="logo-row w100">
+          <div><img src={Chip} className="w70 h55"></img></div>
+          <div><img src={Amex} className="w70 h55"></img></div>
+        </div>
+        <div className="num-row w100 info">
+          <div>####</div><div>####</div><div>####</div><div>####</div>
+        </div>
+        <div className="info-row w100">
+          <div>
+            <div className="label">
+              Card Holder
+          </div>
+            <div className="info">
+              VASHISHT GUPTA
+          </div>
+          </div>
+          <div>
+            <div className="label">
+              Expires
+          </div>
+            <div className="info">
+              MM/YY
+          </div>
+          </div>
+        </div>
+      </div>
+      <div className="form">
 
+      </div>
     </div>
-  </div>
   </>)
 }
 
